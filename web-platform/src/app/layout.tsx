f801a34040f8ef93f9ad5225fc49b1866c008d89
bhavendra <bhavendrakumar007@gmail.com>
2026-06-16 10:13:07 +0530

    web platform created

---
+++ web-platform/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MediCare - AI-Powered Medical Diagnosis",
+  description: "MediCare is an AI-powered medical diagnosis tool that provides accurate and efficient health assessments. Our advanced algorithms analyze symptoms and medical history to offer personalized insights, helping you make informed decisions about your health. Experience the future of healthcare with MediCare.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
